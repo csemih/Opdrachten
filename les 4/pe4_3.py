f6134724ca89cc71634cd3d4102328a115a23023
Semih <semih.celikkaya@student.hu.nl>
2018-09-15 19:52:17 +0200

    Laatste versie

---
+++ les 4/pe4_3.py
@@ -0,0 +1,5 @@
+uurloon = int(input ('Wat verdien je per uur: '))
+aantaluur = int(input('Hoeveel uur heb je gewerkt: '))
+
+totaal = str(aantaluur) + ' uur werken levert ' + str(uurloon * aantaluur) + ' euro op'
+print(totaal)
