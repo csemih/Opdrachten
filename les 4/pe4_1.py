8824a796edd25204f969cfcda9ce480a209dca63
Semih <semih.celikkaya@student.hu.nl>
2018-09-15 19:45:41 +0200

    Laatste versie

---
+++ les 4/pe4_1.py
@@ -0,0 +1,12 @@
+cijferICOR = 5.5
+cijferPROG = 7
+cijferCSN = 6.5
+gemiddelde = (cijferICOR + cijferPROG + cijferCSN) / 3
+print(gemiddelde)
+
+bedrag = 1*30
+beloning = (cijferICOR + cijferPROG + cijferCSN) * bedrag
+print(beloning)
+
+overzicht = 'Mijn cijfer gemiddeld een' ,gemiddelde, 'levert een beloning van €' ,beloning, 'op!'
+print(overzicht)
