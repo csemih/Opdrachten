709958bc784f01766f593642741bc91b9a3c0335
Semih <semih.celikkaya@student.hu.nl>
2018-09-18 14:34:43 +0200

    Laatste versie

---
+++ les 5/pe5_2.py
@@ -0,0 +1,5 @@
+leeftijd = eval(input('Geef je leeftijd: '))
+paspoort = input('Nederlands paspoort (ja/nee): ')
+
+if leeftijd >= 18 and paspoort == 'ja':
+    print('Gefeliciteerd, je mag stemmen!')
